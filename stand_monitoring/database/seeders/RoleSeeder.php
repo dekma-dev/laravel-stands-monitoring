4d6bf2976380136606f5fb90def1423846964112
dekma-dev <128292224+dekma-dev@users.noreply.github.com>
2023-11-21 15:43:41 +0300

    authorization
    completed auth

---
+++ stand_monitoring/database/seeders/RoleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $operator = new Role();
+        $operator->name = 'Operator';
+        $operator->slug = 'operator';
+        $operator->save();
+
+        $admin = new Role();
+        $admin->name = 'Admin';
+        $admin->slug = 'admin';
+        $admin->save();
+    }
+}
